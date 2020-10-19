22fe18001c4015f09340f01b5b3e2c9c60f29939
Aditeya Baral <aditeya.baral@gmail.com>
2020-10-19 08:43:22 +0530

    add gensim summarization

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
+from gensim.summarization import summarize as gensim_summarize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from bs4 import BeautifulSoup
 from GoogleNews import GoogleNews
@@ -70,33 +71,36 @@ def merge(documents, threshold=0.75):
     return " ".join(final_document)
 
 
-def summarize(corpus, num_sentences=15):
-    sentence_list = sent_tokenize(corpus)
-    formatted_article_text = corpus
-    word_frequencies = dict()
-    for word in word_tokenize(formatted_article_text):
-        if word not in stopwords:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
-            else:
-                word_frequencies[word] += 1
-    maximum_frequncy = max(word_frequencies.values())
+def summarize(corpus, mode='rank', ratio=0.5, num_sentences=15):
+    if mode == "frequency":
+        sentence_list = sent_tokenize(corpus)
+        formatted_article_text = corpus
+        word_frequencies = dict()
+        for word in word_tokenize(formatted_article_text):
+            if word not in stopwords:
+                if word not in word_frequencies.keys():
+                    word_frequencies[word] = 1
+                else:
+                    word_frequencies[word] += 1
+        maximum_frequncy = max(word_frequencies.values())
 
-    for word in word_frequencies.keys():
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+        for word in word_frequencies.keys():
+            word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
 
-    sentence_scores = dict()
-    for sent in sentence_list:
-        words = word_tokenize(sent.lower())
-        count_words = len(words)
-        for word in words:
-            if word in word_frequencies.keys():
-                if count_words < 50:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
-    summary_sentences = heapq.nlargest(
-        num_sentences, sentence_scores, key=sentence_scores.get)
-    summary = " ".join(summary_sentences)
-    return summary
+        sentence_scores = dict()
+        for sent in sentence_list:
+            words = word_tokenize(sent.lower())
+            count_words = len(words)
+            for word in words:
+                if word in word_frequencies.keys():
+                    if count_words < 50:
+                        if sent not in sentence_scores.keys():
+                            sentence_scores[sent] = word_frequencies[word]
+                        else:
+                            sentence_scores[sent] += word_frequencies[word]
+        summary_sentences = heapq.nlargest(
+            num_sentences, sentence_scores, key=sentence_scores.get)
+        summary = " ".join(summary_sentences)
+        return summary
+    else:
+        return gensim_summarize(corpus, ratio)
